2b12789b2f9d121e001fd733bd94709976f34682
kimseungwon <kimseungwon9297@gmail.com>
2018-09-14 00:27:53 +0900

    오늘 할일 끝!

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper/ListWrapper';
-import PostList from 'components/list/PostList/PostList';
-import Pagination from 'components/list/Pagination/Pagination';
+import ListContainer from 'components/list/ListContainer';
 
-
-const ListPage = () => {
+const ListPage = ({match}) => {
+    const { page = 1, tag } = match.params;
+    
     return (
         <PageTemplate>
            <ListWrapper>
-             <PostList/>
-             <Pagination/>
+             <ListContainer
+                page={parseInt(page, 10)}
+                tag={tag}
+             />
            </ListWrapper>
         </PageTemplate>
     );
